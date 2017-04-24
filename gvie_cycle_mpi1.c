da654eb4bdcc186f3e97b34920e8abf4548087d6
Andreas Guillot <and.guillot@gmail.com>
2017-04-24 13:37:56 +0200

    maxime-ready

---
+++ gvie_cycle_mpi1.c
@@ -0,0 +1,6 @@
+/*
+  Andreas Guillot
+  Dans ce fichier chaque processus ne s'occupe aue d'une rangee de lignes.
+  Chaque processus s'arrete quand il devient cyclique.
+  Le programme s'arrete quand tous les processus se sont arretes.
+*/
